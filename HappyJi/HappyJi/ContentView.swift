c871bc475777342d6b3b372b1a7d4a3a93302aa3
hyeyeonie <shy020916@naver.com>
2024-06-17 11:18:38 +0900

    init Project

---
+++ HappyJi/HappyJi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyJi
+//
+//  Created by 신혜연 on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyJi/HappyJi/HappyJiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyJiApp.swift
+//  HappyJi
+//
+//  Created by 신혜연 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HappyJiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
